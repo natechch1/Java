38ee7d377f013ee6001e8378ee52266c157c1441
Hongce Chen <natechc1@my.yorku.ca>
2022-07-20 13:05:43 -0400

    Add files via upload

---
+++ user-center/src/main/java/com/nate/usercenter/Mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.nate.usercenter.Mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nate.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
