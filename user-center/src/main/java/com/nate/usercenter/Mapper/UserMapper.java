7485ff8634e567f904a91d757dcadc1bc8c6d7e2
Hongce Chen <natechc1@my.yorku.ca>
2023-01-28 15:04:02 -0500

    Delete user-center directory

--- user-center/src/main/java/com/nate/usercenter/Mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.nate.usercenter.Mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.nate.usercenter.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
